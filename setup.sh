50078f3a408f83caee4ada74fdbca0c0d6308706
Leena Hai Ha Nguyen <leena.nguyen@shopify.com>
2022-06-08 18:21:20 -0400

    WIP

--- setup.sh
+++ setup.sh
@@ -1,35 +1,27 @@
-#!/bin/bash
-set -e
 
-command_exists() {
-	command -v "$@" >/dev/null 2>&1
-}
+[ ! -d ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Only run nested steps in Spin workspaces.
-if ! [ $SPIN ]; then
-  exit 1
-fi 
-
-echo "Starting Spin setup env"
-
-if ! command_exists zsh; then
-  sudo apt-get install -y zsh
+if [ "$SPIN" ]; then
+  DOTFILES_DIR=~/dotfiles
+else
+  DOTFILES_DIR=~/repos-personal/dotfiles
 fi
 
-if ! command_exists fzf; then
-  sudo apt-get install -y fzf
+if [ -d $DOTFILES_DIR ] && [ ! -L $DOTFILES_DIR ]; then
+  for file in "$DOTFILES_DIR"/dotfiles/*; do
+    base="$(basename "$file")"
+    if [ "$base" == "Brewfile" ]; then f="$base"; else f=".$base"; fi
+    echo "Installing $f..."
+    target=~/"$f"
+    [ -f "$target" ] && mv "$target" "$target.bak"
+    ln -s "$file"  ~/"$f"
+  done
+else
+  echo "Could not find dotfiles directory: $DOTFILES_DIR"
 fi
 
-if [ ! -d "~/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-  ln -sf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k/powerlevel10k.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k.zsh-theme
-  cp ~/dotfiles/.p10k.zsh ~/.p10k.zsh
-
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ "$SPIN" ]; then
+  ./script/spin.sh
+else
+  ./script/mac.sh
 fi
-
-cp ~/dotfiles/.gitconfig ~/.gitconfig
-cp ~/dotfiles/.zshrc ~/.zshrc
-echo "Done!!"
